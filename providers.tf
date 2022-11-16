fb2f9b5d9a206c25355482b29274949bbb2b4b0e
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-16 21:30:37 +0000

    commit

---
+++ providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.12.1"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
