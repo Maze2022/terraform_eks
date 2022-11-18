d04d03c32c69ee8eb5736a9d1e3cd5c1efccf5b0
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-18 18:24:54 +0000

    update

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,6 +1,9 @@
 # -------- eks/outputs.tf
 
 output "cluster_name" {
-    value = aws_eks_cluster.wk22_cluster.name
+  value = aws_eks_cluster.wk22_cluster.name
 }
 
+output "cluster_endpoint" {
+  value = aws_eks_cluster.wk22_cluster.endpoint
+}
