2b0bf0b46e57c0d84b8489a024e6433b8c700d09
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-18 18:25:03 +0000

    update

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,5 +1,21 @@
 #---------- eks/variables.tf
 
-variable "cluster_name"
-variable "cluster_endpoint"
-variable "vpc_id"
+variable "vpc_id" {}
+
+variable "endpoint_public_access" {}
+
+variable "endpoint_private_access" {}
+
+variable "public_subnets" {}
+
+variable "desired_size" {
+    type = number
+}
+variable "max_size" {
+    type = number
+}
+
+variable "min_size" {
+    type = number
+}
+
