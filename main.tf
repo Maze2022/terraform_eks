db755f66fdaf055ca396d1de0396d64379f1a3b8
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-18 18:24:25 +0000

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,21 @@
 # -------- root/main.tf
 
 module "vpc" {
-  source               = "./vpc"
-  vpc_cidr             = "192.168.0.0/16"
-  public_sn_count      = 2
-  public_cidrs         = [for i in range(1, 3, 1) : cidrsubnet("192.168.0.0/16", 8, i)]
+  source                  = "./vpc"
+  vpc_cidr                = "192.168.0.0/16"
+  public_sn_count         = 2
+  public_cidrs            = [for i in range(1, 3, 1) : cidrsubnet("192.168.0.0/16", 8, i)]
   map_public_ip_on_launch = true
-  instancy_tenancy     = default
-  
 }
 
 
 module "eks" {
-  source = "./eks"
-  vpc_id = module.vpc.vpc_id
-  cluster_name = module.eks 
-  
+  source                  = "./eks"
+  vpc_id                  = module.vpc.vpc_id
+  public_subnets          = module.vpc.public_subnets
+  desired_size            = 2
+  max_size                = 3
+  min_size                = 1
+  endpoint_public_access  = true
+  endpoint_private_access = false
 }
