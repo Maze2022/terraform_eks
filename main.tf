983508c203d15b9cb11687dab2766577eefa6852
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-19 20:50:08 +0000

    update

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ module "eks" {
   source                  = "./eks"
   vpc_id                  = module.vpc.vpc_id
   public_subnets          = module.vpc.public_subnets
+  instance_types          = ["t3.small"]
   desired_size            = 2
   max_size                = 3
   min_size                = 1
