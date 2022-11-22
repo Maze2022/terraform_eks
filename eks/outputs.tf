1fbb020e938d467c38d507243a9fdd09a28f3592
Imaze Enabulele <ienabulele84+luit@gmail.com>
2022-11-22 02:00:59 +0000

    update

--- eks/outputs.tf
+++ eks/outputs.tf
@@ -1,9 +1,17 @@
 # -------- eks/outputs.tf
 
+output "cluster_id" {
+  value = aws_eks_cluster.luit_cluster.id
+}
+
 output "cluster_name" {
   value = aws_eks_cluster.luit_cluster.name
 }
 
 output "cluster_endpoint" {
   value = aws_eks_cluster.luit_cluster.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.luit_cluster.certificate_authority[0].data
 }
